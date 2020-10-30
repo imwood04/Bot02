6af4a44b2e68bcbde57a51f5285751e04129a2bc
imwood04 <41460038+imwood04@users.noreply.github.com>
2020-10-30 18:04:56 -0400

    Added bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,19 @@
 import discord
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+client = discord.Client()
 
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
 
-client = MyClient()
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('.hello'):
+        await message.channel.send('Hello!')
+
 client.run('NjY0MDM3NDYxMjE4ODIwMTA2.XhRPEw.MQ_fgVbcKW_-Tl0Td0rf9Gz5V_Q')
+
+# NjY0MDM3NDYxMjE4ODIwMTA2.XhRPEw.MQ_fgVbcKW_-Tl0Td0rf9Gz5V_Q
