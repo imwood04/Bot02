62ed929a84c8ce3a5cb839a38dd54e0f1a6dba44
imwood04 <41460038+imwood04@users.noreply.github.com>
2020-10-30 22:59:48 -0400

    Command Testing

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import discord
 import random
-import os
 from discord.ext import commands
 
 intents = discord.Intents(messages=True, guilds=True, reactions=True, members=True, presences=True)
@@ -12,19 +11,24 @@ client = commands.Bot(command_prefix=".", intents=intents)
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
 @client.event
 async def on_member_join(member):
     print(f'{member} Has joined the Server!')
+
 @client.event
 async def on_member_remove(member):
     print(f'{member} Has left the Server!')
+
 @client.command()
 async def hello(ctx):
     await ctx.send('Hello!')
+
 # Ping Command
 @client.command()
 async def ping(ctx):
     await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+
 # 8ball Command
 @client.command(aliases=['8ball'])
 async def _8ball(ctx, *, question):
@@ -32,21 +36,12 @@ async def _8ball(ctx, *, question):
         'Yes', 'No', "Definitely", "Probably Not", "Possibly", "Don't Count on it"
     ]
     await ctx.send(f'Question: {question}\n\nAnswer: {random.choice(responses)}')
+
 # Purge Command
 @client.command()
 async def purge(ctx, amount=5):
     await ctx.channel.purge(limit=amount)
-# cogs
-@client.command()
-async def load(ctx, extensions):
-    client.load_extension(f'cogs.{extensions}')
-@client.command()
-async def unload(ctx, extensions):
-    client.unload_extension(f'cogs.{extensions[:-3]}')
 
-for filename in os.listdir('./cogs'):
-    if filename .endswith('.py'):
-        client.load_extension(f'cogs {filename}')
 client.run('NjY0MDM3NDYxMjE4ODIwMTA2.XhRPEw.LOwGMDOlV4ugDqRpoV75I6wpDcA')
 
 # NjY0MDM3NDYxMjE4ODIwMTA2.XhRPEw.MQ_fgVbcKW_-Tl0Td0rf9Gz5V_Q
--- cogs/extension1.py
+++
@@ -1,8 +0,0 @@
-import discord
-from discord.ext import commands
-
-class example(commands.Cog):
-    def __int__(self, client):
-        self.client = client
-def setup(client):
-    client.add_cog(example(client))
